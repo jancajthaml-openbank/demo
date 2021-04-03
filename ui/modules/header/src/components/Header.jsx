91e5ec72f79018d9477a03fd3dd1a0c13e6aa04d
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-04-03 09:35:40 +0200

    switch to bluma stylesheet, header + content modules

---
+++ ui/modules/header/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Module } from '@lastui/rocker/platform';
+
+const Header = () => {
+	return (
+		<nav class="navbar has-shadow">
+        <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item" href="../">
+                Open Banking
+              </a>
+            </div>
+        </div>
+    </nav>
+	);
+}
+
+export default Header;
