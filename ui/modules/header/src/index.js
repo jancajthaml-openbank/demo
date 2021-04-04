54ff1022e04075f02bf8e13a580215cb80569d97
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-04-04 21:11:39 +0200

    working with styles and fonts

--- ui/modules/header/src/index.js
+++ ui/modules/header/src/index.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import { registerModule } from '@lastui/rocker/platform';
 
 if (process.env.NODE_ENV === 'development') {
-	require('bulma/bulma.sass');
+	require('bulma/bulma.sass').default.use();
 }
 
 registerModule.call(__SANDBOX_SCOPE__, {
