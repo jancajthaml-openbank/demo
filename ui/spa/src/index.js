02ba742bcf9239a3d0999aa48e4ef85aca53d00d
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-04-05 09:13:15 +0200

    fix issues with font awesome fonts

--- ui/spa/src/index.js
+++ ui/spa/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Main } from '@lastui/rocker/runtime';
 
 import 'bulma/bulma.sass';
-import 'font-awesome/scss/font-awesome.scss';
+import 'font-awesome/css/font-awesome.css';
 
 window.addEventListener('load', (event) => {
 	ReactDOM.render(<Main />, document.getElementById('mount'))
--- ui/spa/webpack.config.js
+++ ui/spa/webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-
 const settings = require('./node_modules/@lastui/rocker/webpack/settings');
-
 const config = require(path.resolve(settings.WEBPACK_ROOT_PATH, 'config/spa.js'));
 
 config.entry = {
