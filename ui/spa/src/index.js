df3ebd1c9f1a35bfb6b4e951ce0cc1fe5a33cb23
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-04-08 08:46:33 +0200

    smaller chunks

--- ui/spa/src/index.js
+++ ui/spa/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Main } from '@lastui/rocker/runtime';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "@lastui/rocker/runtime";
+import { getContext } from "./service";
+import "bulma/bulma.sass";
+import "font-awesome/css/font-awesome.css";
 
-import 'bulma/bulma.sass';
-import 'font-awesome/css/font-awesome.css';
-
-window.addEventListener('load', (event) => {
-	ReactDOM.render(<Main />, document.getElementById('mount'))
-})
+window.addEventListener("load", (event) => {
+	const main = <Main fetchContext={getContext} />;
+	ReactDOM.render(main, document.getElementById("mount"));
+});
