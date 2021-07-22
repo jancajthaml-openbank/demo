e12e1b6a8254619152085010e87b2cac1f74ccf4
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-07-22 21:05:53 +0200

    update rocker

--- appliance/ui/modules/content/src/index.js
+++ appliance/ui/modules/content/src/index.js
@@ -3,10 +3,11 @@ import Content from './components/Content';
 import { registerModule } from '@lastui/rocker/platform';
 
 if (process.env.NODE_ENV === 'development') {
-	require('bulma/bulma.sass').default.use();
+	require('bulma/bulma.sass');
 }
 
+import './styles/index.css'
+
 registerModule.call(__SANDBOX_SCOPE__, {
-	MainView: Content,
-	styles: require('./styles/index.css').default,
+	Main: Content,
 });
--- appliance/ui/modules/layout/src/index.js
+++ appliance/ui/modules/layout/src/index.js
@@ -2,10 +2,6 @@ import Layout from './components/Layout';
 
 import { registerModule } from '@lastui/rocker/platform';
 
-if (process.env.NODE_ENV === 'development') {
-	require('bulma/bulma.sass');
-}
-
 registerModule.call(__SANDBOX_SCOPE__, {
-	MainView: Layout,
+	Main: Layout,
 });
