36d8cb1ccd2de568a8f7fb3e5daf1671a81947ab
Jan Cajthaml <jan.cajthaml@gmail.com>
2021-07-23 04:10:22 +0200

    reactive navigation

--- appliance/ui/spa/src/index.js
+++ appliance/ui/spa/src/index.js
@@ -5,7 +5,9 @@ import { getContext } from "./service";
 import "bulma/bulma.sass";
 import "font-awesome/css/font-awesome.css";
 
-window.addEventListener("load", (event) => {
+document.title = "Openbank Demo"
+
+window.addEventListener("load", (event) => {	
 	const main = <Main fetchContext={getContext} />;
 	ReactDOM.render(main, document.getElementById("mount"));
 });
